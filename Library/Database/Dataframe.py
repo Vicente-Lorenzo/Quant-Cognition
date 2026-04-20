43fd8a315a3dc4b13df0d88af21755bc01586a36
Vicente Lorenzo <vicente.aser.lorenzo@gmail.com>
2026-04-20 18:38:16 +0100

    [Database] Minor Fixes
    Signed-off-by: Vicente Lorenzo <vicente.aser.lorenzo@gmail.com>

--- Library/Database/Dataframe.py
+++ Library/Database/Dataframe.py
@@ -1,7 +1,18 @@
 from typing import Any
 
+import numpy as np
+np.set_printoptions(threshold=1000, linewidth=1000)
 import pandas as pd
+pd.options.display.max_rows = None
+pd.options.display.max_columns = None
+pd.options.display.width = 0
+pd.options.display.max_colwidth = None
 import polars as pl
+pl.Config.set_tbl_cols(-1)
+pl.Config.set_tbl_rows(-1)
+pl.Config.set_tbl_width_chars(1000)
+pl.Config.set_fmt_str_lengths(1000)
+pl.Config.set_fmt_table_cell_list_len(-1)
 
 from Library.Utility.Typing import MISSING, Missing
 from Library.Database.Dataclass import DataclassAPI
