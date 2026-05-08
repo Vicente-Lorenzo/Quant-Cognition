bfad16194479627be638f5ca80df8360a23c8035
Vicente Lorenzo <vicente.aser.lorenzo@gmail.com>
2026-05-08 19:18:40 +0100

    [Market] Merged Indicator
    Signed-off-by: Vicente Lorenzo <vicente.aser.lorenzo@gmail.com>

--- Library/Indicator/Technical/__init__.py
+++ Library/Indicator/Technical/__init__.py
@@ -1,15 +1,15 @@
 from Library.Indicator.Technical.Technical import TechnicalAPI, TechnicalType
 
-import Library.Indicator.Technical.Baseline as Baseline 
+from Library.Indicator.Technical import Baseline
 from Library.Indicator.Technical.Baseline import *
 
-import Library.Indicator.Technical.Overlap as Overlap
+from Library.Indicator.Technical import Overlap
 from Library.Indicator.Technical.Overlap import *
 
-import Library.Indicator.Technical.Volatility as Volatility
+from Library.Indicator.Technical import Volatility
 from Library.Indicator.Technical.Volatility import *
 
-import Library.Indicator.Technical.Momentum as Momentum
+from Library.Indicator.Technical import Momentum
 from Library.Indicator.Technical.Momentum import *
 
 __all__ = [
--- Library/Indicator/__init__.py
+++ Library/Indicator/__init__.py
@@ -1,7 +1,25 @@
-from Library.Indicator.Configuration import IndicatorType, IndicatorMode, IndicatorConfigurationAPI
+from Library.Indicator.Indicator import (
+    IndicatorMode,
+    parse_technical,
+    parse_fundamental,
+    parse_sentimental
+)
+
+from Library.Indicator import Technical
+from Library.Indicator.Technical import *
+
+from Library.Indicator import Fundamental
+from Library.Indicator.Fundamental import *
+
+from Library.Indicator import Sentimental
+from Library.Indicator.Sentimental import *
 
 __all__ = [
-    "IndicatorType",
     "IndicatorMode",
-    "IndicatorConfigurationAPI"
+    "parse_technical",
+    "parse_fundamental",
+    "parse_sentimental",
+    *Technical.__all__,
+    *Fundamental.__all__,
+    *Sentimental.__all__
 ]
