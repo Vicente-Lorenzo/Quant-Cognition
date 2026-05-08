ce7a7e4fbdf69f6b0950a6392c2e595a1fe9f81d
Vicente Lorenzo <vicente.aser.lorenzo@gmail.com>
2026-05-08 19:11:47 +0100

    [Market] Merged Indicator
    Signed-off-by: Vicente Lorenzo <vicente.aser.lorenzo@gmail.com>

--- Library/Indicator/Technical/__init__.py
+++ Library/Indicator/Technical/__init__.py
@@ -1,11 +1,15 @@
 from Library.Indicator.Technical.Technical import TechnicalAPI, TechnicalType
-from Library.Indicator.Technical import Baseline
+
+import Library.Indicator.Technical.Baseline as Baseline 
 from Library.Indicator.Technical.Baseline import *
-from Library.Indicator.Technical import Overlap
+
+import Library.Indicator.Technical.Overlap as Overlap
 from Library.Indicator.Technical.Overlap import *
-from Library.Indicator.Technical import Volatility
+
+import Library.Indicator.Technical.Volatility as Volatility
 from Library.Indicator.Technical.Volatility import *
-from Library.Indicator.Technical import Momentum
+
+import Library.Indicator.Technical.Momentum as Momentum
 from Library.Indicator.Technical.Momentum import *
 
 __all__ = [
