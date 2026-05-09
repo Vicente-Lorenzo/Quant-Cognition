b853582e1b2ba392a102f8205bf13dd7bddbf8f2
Vicente Lorenzo <vicente.aser.lorenzo@gmail.com>
2026-05-09 16:04:57 +0100

    [Market] Merged Indicator
    Signed-off-by: Vicente Lorenzo <vicente.aser.lorenzo@gmail.com>

--- Library/Indicator/__init__.py
+++ Library/Indicator/__init__.py
@@ -5,13 +5,13 @@ from Library.Indicator.Indicator import (
     parse_sentimental
 )
 
-from Library.Indicator import Technical
+from Library.Indicator.Technical import __all__ as technical
 from Library.Indicator.Technical import *
 
-from Library.Indicator import Fundamental
+from Library.Indicator.Fundamental import __all__ as fundamental
 from Library.Indicator.Fundamental import *
 
-from Library.Indicator import Sentimental
+from Library.Indicator.Sentimental import __all__ as sentimental
 from Library.Indicator.Sentimental import *
 
 __all__ = [
@@ -19,7 +19,7 @@ __all__ = [
     "parse_technical",
     "parse_fundamental",
     "parse_sentimental",
-    *Technical.__all__,
-    *Fundamental.__all__,
-    *Sentimental.__all__
+    *technical,
+    *fundamental,
+    *sentimental
 ]
