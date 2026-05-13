47bef940624d6dd37ba9bce1b8b810653d9d3d81
Vicente Lorenzo <vicente.aser.lorenzo@gmail.com>
2026-05-13 19:59:51 +0100

    [System] Started Implementing Realtime
    Signed-off-by: Vicente Lorenzo <vicente.aser.lorenzo@gmail.com>

---
+++ Sources/Indicators/Connector/Connector/Connector.cs
@@ -0,0 +1,31 @@
+using System;
+using cAlgo.API;
+using cAlgo.API.Collections;
+using cAlgo.API.Indicators;
+using cAlgo.API.Internals;
+
+namespace cAlgo.Indicators;
+
+[Indicator(AccessRights = AccessRights.None)]
+public class Connector : Indicator
+{
+    [Parameter(DefaultValue = "Hello world!")]
+    public string Message { get; set; }
+
+    [Output("Main")]
+    public IndicatorDataSeries Result { get; set; }
+
+    protected override void Initialize()
+    {
+        // To learn more about cTrader Algo visit our Help Center:
+        // https://help.ctrader.com/ctrader-algo/
+
+        Print(Message);
+    }
+
+    public override void Calculate(int index)
+    {
+        // Calculate value at specified index
+        // Result[index] = 
+    }
+}
