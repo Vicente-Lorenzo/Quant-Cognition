0d966ae259d125cb71ad36ad18f3f3fca3127fc3
Vicente Lorenzo <vicente.aser.lorenzo@gmail.com>
2026-05-19 23:23:05 +0100

    [Parameter] Improved Parameter Module
    Signed-off-by: Vicente Lorenzo <vicente.aser.lorenzo@gmail.com>

--- Library/Strategy/Model/DDPG.py
+++ Library/Strategy/Model/DDPG.py
@@ -1,16 +1,15 @@
 from typing import Union
-from Library.Parameters import Parameters
+from Library.Parameter import Parameter
 
 from Library.Engine import MachineAPI
 from Library.Strategy import StrategyAPI
-from Library.Model.DDPG import DDPGAgentAPI
 
 class DDPGStrategyAPI(StrategyAPI):
 
     def __init__(self,
-                 money_management: Parameters,
-                 risk_management: Parameters,
-                 signal_management: Parameters):
+                 money_management: Parameter,
+                 risk_management: Parameter,
+                 signal_management: Parameter):
 
         super().__init__(money_management, risk_management, signal_management)
 
