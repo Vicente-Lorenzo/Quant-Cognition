d3c84992878a7e1f7cba9bd61a74341adb786e15
Vicente Lorenzo <vicente.aser.lorenzo@gmail.com>
2026-05-21 18:34:17 +0100

    [System] Improved Realtime System
    Signed-off-by: Vicente Lorenzo <vicente.aser.lorenzo@gmail.com>

--- Scripts/gemini.bat
+++ Scripts/gemini.bat
@@ -6,7 +6,7 @@ echo -----------------------------------------------
 
 echo [1/1] Launching Antigravity in project directory...
 cd /d "C:\Users\Admin\OneDrive\Documents\cAlgo"
-agy
+gemini
 
 echo -----------------------------------------------
 echo Antigravity CLI Terminated.
