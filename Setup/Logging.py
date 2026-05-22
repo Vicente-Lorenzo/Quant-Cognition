a62e3ec31f00dbb6dd87e74a3e9e91b066ad4762
Vicente Lorenzo <vicente.aser.lorenzo@gmail.com>
2026-05-22 02:17:46 +0100

    [System] Improved Connector cBot
    Signed-off-by: Vicente Lorenzo <vicente.aser.lorenzo@gmail.com>

--- Setup/Logging.py
+++ Setup/Logging.py
@@ -5,13 +5,12 @@ sys.path.insert(0, str(Path(__file__).parent.parent))
 
 from Library.Logging import HandlerLoggingAPI
 from Library.Logging.Logging import VerboseLevel
-from Setup.Enum import enum_block, write_enum_file
+from Setup.Enum import enum_block, write_all
 
 def logging_block() -> str:
     return enum_block("VerboseLevel", [(v.name, v.value) for v in VerboseLevel])
 
 if __name__ == "__main__":
-    from Setup.Strategy import strategy_block
     with HandlerLoggingAPI() as logger:
-        path = write_enum_file([strategy_block(), logging_block()])
+        path = write_all()
         logger.info(f"Generated {path}")
