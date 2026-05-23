9cdd7c46010e511b01c12f3bd5747519d417187d
Vicente Lorenzo <vicente.aser.lorenzo@gmail.com>
2026-05-23 16:57:43 +0100

    [System] Improved Connector cBot
    Signed-off-by: Vicente Lorenzo <vicente.aser.lorenzo@gmail.com>

--- Sources/Robots/Connector/Connector/Connector.cs
+++ Sources/Robots/Connector/Connector/Connector.cs
@@ -6,23 +6,56 @@ using cAlgo.API.Internals;
 
 namespace cAlgo.Robots;
 
-[Robot(AccessRights = AccessRights.None, AddIndicators = true)]
+[Robot(AccessRights = AccessRights.FullAccess, AddIndicators = true)]
 public class Connector : Robot
 {
-    [Parameter("Strategy", Group = "Strategy Management", DefaultValue = StrategyType.Download)]
-    public StrategyType Strategy { get; set; }
-
     [Parameter("Console", Group = "Logging Management", DefaultValue = VerboseLevel.Debug)]
     public VerboseLevel Console { get; set; }
 
     [Parameter("File", Group = "Logging Management", DefaultValue = VerboseLevel.Debug)]
     public VerboseLevel File { get; set; }
 
+    [Parameter("Strategy", Group = "Strategy Management", DefaultValue = StrategyType.Download)]
+    public StrategyType Strategy { get; set; }
+
+    [Parameter("Database", Group = "System Management", DefaultValue = DatabaseType.Auto)]
+    public DatabaseType Database { get; set; }
+
+    [Parameter("Verification", Group = "System Management", DefaultValue = 3, MinValue = 1)]
+    public int Verification { get; set; }
+
+    [Parameter("Tick Stream", Group = "System Management", DefaultValue = TickStreamMode.Auto)]
+    public TickStreamMode TickStream { get; set; }
+
+    [Parameter("Bar Stream", Group = "System Management", DefaultValue = StreamMode.Auto)]
+    public StreamMode BarStream { get; set; }
+
+    [Parameter("Order Stream", Group = "System Management", DefaultValue = StreamMode.Auto)]
+    public StreamMode OrderStream { get; set; }
+
+    [Parameter("Position Stream", Group = "System Management", DefaultValue = StreamMode.Auto)]
+    public StreamMode PositionStream { get; set; }
+
+    [Parameter("Trade Stream", Group = "System Management", DefaultValue = StreamMode.Auto)]
+    public StreamMode TradeStream { get; set; }
+
+    [Parameter("Market Batch", Group = "Buffering Management", DefaultValue = -1, MinValue = -1)]
+    public int MarketBatch { get; set; }
+
+    [Parameter("Market Interval", Group = "Buffering Management", DefaultValue = -1.0, MinValue = -1.0)]
+    public double MarketInterval { get; set; }
+
+    [Parameter("Portfolio Batch", Group = "Buffering Management", DefaultValue = -1, MinValue = -1)]
+    public int PortfolioBatch { get; set; }
+
+    [Parameter("Portfolio Interval", Group = "Buffering Management", DefaultValue = -1.0, MinValue = -1.0)]
+    public double PortfolioInterval { get; set; }
+
     private RobotAPI _robot_api_;
 
     protected override void OnStart()
     {
-        _robot_api_ = new RobotAPI(this, Console, File, Strategy);
+        _robot_api_ = new RobotAPI(this, Console, File, Strategy, Database, Verification, TickStream, BarStream, OrderStream, PositionStream, TradeStream, MarketBatch, MarketInterval, PortfolioBatch, PortfolioInterval);
     }
 
     protected override void OnStop()
@@ -30,13 +63,13 @@ public class Connector : Robot
         _robot_api_?.OnShutdown();
         _robot_api_?.Dispose();
     }
-    
+
     protected override void OnException(Exception exception)
     {
         _robot_api_?.OnException(exception);
         base.OnException(exception);
     }
-    
+
     protected override void OnError(Error error)
     {
         _robot_api_?.OnError(error);
