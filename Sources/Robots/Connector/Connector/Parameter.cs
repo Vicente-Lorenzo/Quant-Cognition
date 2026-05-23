dc591cbeaefc59afb5733f868e12e111da83bc05
Vicente Lorenzo <vicente.aser.lorenzo@gmail.com>
2026-05-23 17:10:01 +0100

    [System] Improved Connector cBot
    Signed-off-by: Vicente Lorenzo <vicente.aser.lorenzo@gmail.com>

---
+++ Sources/Robots/Connector/Connector/Parameter.cs
@@ -0,0 +1,24 @@
+namespace cAlgo.Robots;
+
+public enum DatabaseType
+{
+    Auto = 0,
+    Quant = 1,
+    Tests = 2,
+    Off = 3,
+}
+
+public enum TickStreamMode
+{
+    Auto = 0,
+    All = 1,
+    Target = 2,
+    Off = 3,
+}
+
+public enum StreamMode
+{
+    Auto = 0,
+    All = 1,
+    Off = 2,
+}
