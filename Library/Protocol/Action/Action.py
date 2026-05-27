77b2bc8cb63ea59f084c9fbc1d793ce159fa7c65
Vicente Lorenzo <vicente.aser.lorenzo@gmail.com>
2026-05-27 22:35:34 +0100

    [System] Improved Connector cBot
    Signed-off-by: Vicente Lorenzo <vicente.aser.lorenzo@gmail.com>

--- Library/Protocol/Action/Action.py
+++ Library/Protocol/Action/Action.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 from typing import Union, ClassVar
 from dataclasses import dataclass
 
+from Library.Protocol.Binary import BinaryAPI
 from Library.Database.Dataclass import DataclassAPI
 from Library.Utility.Enumeration import EnumerationAPI
 from Library.Utility.Typing import cast
@@ -66,40 +67,55 @@ class ActionID(EnumerationAPI):
 @dataclass(slots=True)
 class ActionAPI(DataclassAPI):
     ActionID: ClassVar[ActionID]
-    def serialize(self) -> str:
-        return self.json(include_initvar_fields=True, ActionID=self.ActionID.value)
+    def serialize(self) -> bytes:
+        raise NotImplementedError
 
 @dataclass(slots=True)
 class CompleteActionAPI(ActionAPI):
     ActionID: ClassVar[ActionID] = ActionID.Complete
+    _binary_: ClassVar[BinaryAPI] = BinaryAPI('B')
+    def serialize(self) -> bytes:
+        return self._binary_.pack(self.ActionID.value)
 
 @dataclass(slots=True)
 class AskAboveTargetActionAPI(ActionAPI):
     ActionID: ClassVar[ActionID] = ActionID.AskAboveTarget
+    _binary_: ClassVar[BinaryAPI] = BinaryAPI('B', 'D')
     Ask: Union[float, None]
     def __post_init__(self):
         self.Ask = cast(self.Ask, float, None)
+    def serialize(self) -> bytes:
+        return self._binary_.pack(self.ActionID.value, self.Ask)
 
 @dataclass(slots=True)
 class AskBelowTargetActionAPI(ActionAPI):
     ActionID: ClassVar[ActionID] = ActionID.AskBelowTarget
+    _binary_: ClassVar[BinaryAPI] = BinaryAPI('B', 'D')
     Ask: Union[float, None]
     def __post_init__(self):
         self.Ask = cast(self.Ask, float, None)
+    def serialize(self) -> bytes:
+        return self._binary_.pack(self.ActionID.value, self.Ask)
 
 @dataclass(slots=True)
 class BidAboveTargetActionAPI(ActionAPI):
     ActionID: ClassVar[ActionID] = ActionID.BidAboveTarget
+    _binary_: ClassVar[BinaryAPI] = BinaryAPI('B', 'D')
     Bid: Union[float, None]
     def __post_init__(self):
         self.Bid = cast(self.Bid, float, None)
+    def serialize(self) -> bytes:
+        return self._binary_.pack(self.ActionID.value, self.Bid)
 
 @dataclass(slots=True)
 class BidBelowTargetActionAPI(ActionAPI):
     ActionID: ClassVar[ActionID] = ActionID.BidBelowTarget
+    _binary_: ClassVar[BinaryAPI] = BinaryAPI('B', 'D')
     Bid: Union[float, None]
     def __post_init__(self):
         self.Bid = cast(self.Bid, float, None)
+    def serialize(self) -> bytes:
+        return self._binary_.pack(self.ActionID.value, self.Bid)
 
 __all__ = [
     "ActionID",
