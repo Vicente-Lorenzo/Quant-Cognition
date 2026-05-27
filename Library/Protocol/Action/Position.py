a2692cfac21bc6c91074dc5f993ccb67c41c7bff
Vicente Lorenzo <vicente.aser.lorenzo@gmail.com>
2026-05-27 22:36:29 +0100

    [System] Improved Connector cBot
    Signed-off-by: Vicente Lorenzo <vicente.aser.lorenzo@gmail.com>

--- Library/Protocol/Action/Position.py
+++ Library/Protocol/Action/Position.py
@@ -3,13 +3,15 @@ from __future__ import annotations
 from typing import Union, ClassVar
 from dataclasses import dataclass
 
-from Library.Protocol.Action.Action import ActionAPI, ActionID
-from Library.Portfolio.Position import PositionType
 from Library.Utility.Typing import cast
+from Library.Protocol.Binary import BinaryAPI
+from Library.Portfolio.Position import PositionType
+from Library.Protocol.Action.Action import ActionAPI, ActionID
 
 @dataclass(slots=True)
 class OpenBuyPositionActionAPI(ActionAPI):
     ActionID: ClassVar[ActionID] = ActionID.OpenBuyPosition
+    _binary_: ClassVar[BinaryAPI] = BinaryAPI('B', 's', 'd', 'D', 'D')
     PositionType: PositionType
     Volume: float
     StopLoss: Union[float, None]
@@ -18,10 +20,13 @@ class OpenBuyPositionActionAPI(ActionAPI):
         self.PositionType = PositionType(self.PositionType)
         self.StopLoss = cast(self.StopLoss, float, None)
         self.TakeProfit = cast(self.TakeProfit, float, None)
+    def serialize(self) -> bytes:
+        return self._binary_.pack(self.ActionID.value, self.PositionType.name, self.Volume, self.StopLoss, self.TakeProfit)
 
 @dataclass(slots=True)
 class OpenSellPositionActionAPI(ActionAPI):
     ActionID: ClassVar[ActionID] = ActionID.OpenSellPosition
+    _binary_: ClassVar[BinaryAPI] = BinaryAPI('B', 's', 'd', 'D', 'D')
     PositionType: PositionType
     Volume: float
     StopLoss: Union[float, None]
@@ -30,60 +35,86 @@ class OpenSellPositionActionAPI(ActionAPI):
         self.PositionType = PositionType(self.PositionType)
         self.StopLoss = cast(self.StopLoss, float, None)
         self.TakeProfit = cast(self.TakeProfit, float, None)
+    def serialize(self) -> bytes:
+        return self._binary_.pack(self.ActionID.value, self.PositionType.name, self.Volume, self.StopLoss, self.TakeProfit)
 
 @dataclass(slots=True)
 class ModifyBuyPositionVolumeActionAPI(ActionAPI):
     ActionID: ClassVar[ActionID] = ActionID.ModifyBuyPositionVolume
+    _binary_: ClassVar[BinaryAPI] = BinaryAPI('B', 'i', 'd')
     PositionID: int
     Volume: float
+    def serialize(self) -> bytes:
+        return self._binary_.pack(self.ActionID.value, self.PositionID, self.Volume)
 
 @dataclass(slots=True)
 class ModifySellPositionVolumeActionAPI(ActionAPI):
     ActionID: ClassVar[ActionID] = ActionID.ModifySellPositionVolume
+    _binary_: ClassVar[BinaryAPI] = BinaryAPI('B', 'i', 'd')
     PositionID: int
     Volume: float
+    def serialize(self) -> bytes:
+        return self._binary_.pack(self.ActionID.value, self.PositionID, self.Volume)
 
 @dataclass(slots=True)
 class ModifyBuyPositionStopLossActionAPI(ActionAPI):
     ActionID: ClassVar[ActionID] = ActionID.ModifyBuyPositionStopLoss
+    _binary_: ClassVar[BinaryAPI] = BinaryAPI('B', 'i', 'D')
     PositionID: int
     StopLoss: Union[float, None]
     def __post_init__(self):
         self.StopLoss = cast(self.StopLoss, float, None)
+    def serialize(self) -> bytes:
+        return self._binary_.pack(self.ActionID.value, self.PositionID, self.StopLoss)
 
 @dataclass(slots=True)
 class ModifySellPositionStopLossActionAPI(ActionAPI):
     ActionID: ClassVar[ActionID] = ActionID.ModifySellPositionStopLoss
+    _binary_: ClassVar[BinaryAPI] = BinaryAPI('B', 'i', 'D')
     PositionID: int
     StopLoss: Union[float, None]
     def __post_init__(self):
         self.StopLoss = cast(self.StopLoss, float, None)
+    def serialize(self) -> bytes:
+        return self._binary_.pack(self.ActionID.value, self.PositionID, self.StopLoss)
 
 @dataclass(slots=True)
 class ModifyBuyPositionTakeProfitActionAPI(ActionAPI):
     ActionID: ClassVar[ActionID] = ActionID.ModifyBuyPositionTakeProfit
+    _binary_: ClassVar[BinaryAPI] = BinaryAPI('B', 'i', 'D')
     PositionID: int
     TakeProfit: Union[float, None]
     def __post_init__(self):
         self.TakeProfit = cast(self.TakeProfit, float, None)
+    def serialize(self) -> bytes:
+        return self._binary_.pack(self.ActionID.value, self.PositionID, self.TakeProfit)
 
 @dataclass(slots=True)
 class ModifySellPositionTakeProfitActionAPI(ActionAPI):
     ActionID: ClassVar[ActionID] = ActionID.ModifySellPositionTakeProfit
+    _binary_: ClassVar[BinaryAPI] = BinaryAPI('B', 'i', 'D')
     PositionID: int
     TakeProfit: Union[float, None]
     def __post_init__(self):
         self.TakeProfit = cast(self.TakeProfit, float, None)
+    def serialize(self) -> bytes:
+        return self._binary_.pack(self.ActionID.value, self.PositionID, self.TakeProfit)
 
 @dataclass(slots=True)
 class CloseBuyPositionActionAPI(ActionAPI):
     ActionID: ClassVar[ActionID] = ActionID.CloseBuyPosition
+    _binary_: ClassVar[BinaryAPI] = BinaryAPI('B', 'i')
     PositionID: int
+    def serialize(self) -> bytes:
+        return self._binary_.pack(self.ActionID.value, self.PositionID)
 
 @dataclass(slots=True)
 class CloseSellPositionActionAPI(ActionAPI):
     ActionID: ClassVar[ActionID] = ActionID.CloseSellPosition
+    _binary_: ClassVar[BinaryAPI] = BinaryAPI('B', 'i')
     PositionID: int
+    def serialize(self) -> bytes:
+        return self._binary_.pack(self.ActionID.value, self.PositionID)
 
 __all__ = [
     "OpenBuyPositionActionAPI",
