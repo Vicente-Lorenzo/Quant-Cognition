1778aac9ccb2b2a5640ac79e03e0c66f8f5c2cdf
Vicente Lorenzo <vicente.aser.lorenzo@gmail.com>
2026-05-28 00:50:43 +0100

    [System] Improved Connector cBot
    Signed-off-by: Vicente Lorenzo <vicente.aser.lorenzo@gmail.com>

--- Sources/Robots/Connector/Connector/System.cs
+++ Sources/Robots/Connector/Connector/System.cs
@@ -1,232 +1,213 @@
-using NetMQ;
 using System;
+using System.IO;
+using System.Text;
 using cAlgo.API;
-using System.Diagnostics;
-using NetMQ.Sockets;
-using Newtonsoft.Json;
 using cAlgo.API.Internals;
 
-namespace cAlgo.Robots;
+namespace Connector;
 
 public class SystemAPI : IDisposable
 {
-    private readonly Logging _console_;
-    private readonly PairSocket _socket_;
-    private bool _disposed_;
-    private Process _peer_;
-    private volatile bool _peer_dead_;
-    private static readonly TimeSpan _poll_interval_ = TimeSpan.FromMilliseconds(100);
+    private readonly TransportAPI _transport_;
 
-    public SystemAPI(Robot robot, VerboseLevel console, string host = "localhost", int port = 5555)
+    public SystemAPI(Robot robot, VerboseLevel console, string iid)
     {
-        _console_ = new Logging(robot, "API", console);
-        _socket_ = new PairSocket();
-        _socket_.Options.SendHighWatermark = 0;
-        _socket_.Options.Linger = TimeSpan.Zero;
-        _socket_.Connect($"tcp://{host}:{port}");
-        _console_.Info($"ZMQ PAIR Socket connected to tcp://{host}:{port}");
+        _transport_ = new TransportAPI(robot, console, iid);
     }
 
-    public void SetPeer(Process peer)
+    public bool PeerDead => _transport_.PeerDead;
+
+    public void Watchdog(System.Diagnostics.Process peer) => _transport_.Watchdog(peer);
+
+    private void Send(byte[] data) => _transport_.Send(data);
+
+    public byte[] Receive() => _transport_.Receive();
+
+    public void Dispose() => _transport_.Dispose();
+
+    private static void WriteString(MemoryStream ms, string value)
     {
-        _peer_ = peer;
-        if (peer == null) return;
-        System.Threading.Tasks.Task.Run(() =>
+        if (value == null)
         {
-            try { peer.WaitForExit(); }
-            catch (Exception e) { _console_.Warning($"Peer watch failed: {e.Message}"); }
-            OnPeerExited();
-        });
-    }
-
-    private void OnPeerExited()
-    {
-        _peer_dead_ = true;
-        _console_.Warning($"Python process exited (code {_peer_?.ExitCode}); receive/send loops will unblock");
-    }
-
-    public void Connect()
-    {
-        _console_.Info("ZMQ Ready for communication");
-    }
-
-    public void Disconnect()
-    {
-        if (_disposed_) return;
-        try { _socket_.Close(); } catch (Exception e) { _console_.Warning($"Socket close: {e.Message}"); }
-        _console_.Info("Disconnected");
-    }
-
-    public void Dispose()
-    {
-        if (_disposed_) return;
-        _disposed_ = true;
-        try { _socket_.Dispose(); } catch (Exception e) { _console_.Warning($"Socket dispose: {e.Message}"); }
-    }
-
-    private void SendUpdate(object payload)
-    {
-        if (_peer_dead_) throw new InvalidOperationException("Python process exited; cannot send");
-        var json = JsonConvert.SerializeObject(payload);
-        while (!_socket_.TrySendFrame(_poll_interval_, json))
-        {
-            if (_peer_dead_) throw new InvalidOperationException("Python process exited during send");
+            ms.Write(BitConverter.GetBytes((ushort)0), 0, 2);
+            return;
         }
+        byte[] encoded = Encoding.UTF8.GetBytes(value);
+        ms.Write(BitConverter.GetBytes((ushort)encoded.Length), 0, 2);
+        ms.Write(encoded, 0, encoded.Length);
+    }
+
+    private static double NanIfNull(double? value)
+    {
+        return value ?? double.NaN;
+    }
+
+    private static PositionTypeID ParsePositionType(string comment)
+    {
+        if (comment == null) return PositionTypeID.Normal;
+        if (Enum.TryParse<PositionTypeID>(comment, out var result)) return result;
+        return PositionTypeID.Normal;
+    }
+
+    public void SendUpdateInitialization(int pid)
+    {
+        using var ms = new MemoryStream();
+        ms.WriteByte((byte)UpdateID.Initialization);
+        ms.Write(BitConverter.GetBytes(pid), 0, 4);
+        Send(ms.ToArray());
     }
 
     public void SendUpdateComplete()
     {
-        SendUpdate(new { UpdateID = (int)UpdateID.Complete });
-    }
-
-    public void SendUpdateAccount(IAccount account)
-    {
-        SendUpdate(new
-        {
-            UpdateID = (int)UpdateID.Account,
-            AccountType = (int)account.AccountType,
-            AssetType = account.Asset.Name,
-            Balance = account.Balance,
-            Equity = account.Equity,
-            Credit = account.Credit,
-            Leverage = account.PreciseLeverage,
-            MarginUsed = account.Margin,
-            MarginFree = account.FreeMargin,
-            MarginLevel = account.MarginLevel,
-            MarginStopLevel = account.StopOutLevel,
-            MarginMode = (int)account.TotalMarginCalculationType
-        });
-    }
-
-    public void SendUpdateSymbol(Symbol symbol)
-    {
-        SendUpdate(new
-        {
-            UpdateID = (int)UpdateID.Security,
-            BaseAssetType = symbol.BaseAsset.Name,
-            QuoteAssetType = symbol.QuoteAsset.Name,
-            Digits = symbol.Digits,
-            PointSize = symbol.TickSize,
-            PipSize = symbol.PipSize,
-            LotSize = symbol.LotSize,
-            VolumeInUnitsMin = symbol.VolumeInUnitsMin,
-            VolumeInUnitsMax = symbol.VolumeInUnitsMax,
-            VolumeInUnitsStep = symbol.VolumeInUnitsStep,
-            Commission = symbol.Commission,
-            CommissionMode = (int)symbol.CommissionType,
-            SwapLong = symbol.SwapLong,
-            SwapShort = symbol.SwapShort,
-            SwapMode = (int)symbol.SwapCalculationType,
-            SwapExtraDay = symbol.Swap3DaysRollover != null ? (int)symbol.Swap3DaysRollover : 0
-        });
-    }
-
-    public void SendUpdatePosition(UpdateID update_id, Position position)
-    {
-        SendUpdate(new
-        {
-            UpdateID = (int)update_id,
-            PositionID = position.Id,
-            PositionType = position.Comment,
-            TradeType = (int)position.TradeType,
-            EntryTimestamp = ((DateTimeOffset)position.EntryTime).ToUnixTimeMilliseconds(),
-            EntryPrice = position.EntryPrice,
-            StopLoss = position.StopLoss,
-            TakeProfit = position.TakeProfit,
-            Volume = position.VolumeInUnits,
-            Quantity = position.Quantity,
-            GrossPnL = position.GrossProfit,
-            CommissionPnL = position.Commissions,
-            SwapPnL = position.Swap,
-            NetPnL = position.NetProfit,
-            UsedMargin = position.Margin
-        });
-    }
-
-    public void SendUpdateTrade(UpdateID update_id, HistoricalTrade trade)
-    {
-        SendUpdate(new
-        {
-            UpdateID = (int)update_id,
-            PositionID = trade.PositionId,
-            TradeID = trade.ClosingDealId,
-            PositionType = trade.Comment,
-            TradeType = (int)trade.TradeType,
-            EntryTimestamp = ((DateTimeOffset)trade.EntryTime).ToUnixTimeMilliseconds(),
-            ExitTimestamp = ((DateTimeOffset)trade.ClosingTime).ToUnixTimeMilliseconds(),
-            EntryPrice = trade.EntryPrice,
-            ExitPrice = trade.ClosingPrice,
-            Volume = trade.VolumeInUnits,
-            Quantity = trade.Quantity,
-            GrossPnL = trade.GrossProfit,
-            CommissionPnL = trade.Commissions,
-            SwapPnL = trade.Swap,
-            NetPnL = trade.NetProfit
-        });
-    }
-    
-    public void SendUpdateBarClosed(RobotAPI.xBar bar)
-    {
-        SendUpdate(new
-        {
-            UpdateID = (int)UpdateID.BarClosed,
-            Timestamp = ((DateTimeOffset)bar.Timestamp).ToUnixTimeMilliseconds(),
-            GapAsk = bar.GapTick.Ask, GapBid = bar.GapTick.Bid,
-            GapTimestamp = ((DateTimeOffset)bar.GapTick.Timestamp).ToUnixTimeMilliseconds(),
-            GapVolume = bar.GapTick.Volume,
-            GapAskBaseConversion = bar.GapTick.AskBaseConversion, GapBidBaseConversion = bar.GapTick.BidBaseConversion,
-            GapAskQuoteConversion = bar.GapTick.AskQuoteConversion, GapBidQuoteConversion = bar.GapTick.BidQuoteConversion,
-            OpenAsk = bar.OpenTick.Ask, OpenBid = bar.OpenTick.Bid,
-            OpenTimestamp = ((DateTimeOffset)bar.OpenTick.Timestamp).ToUnixTimeMilliseconds(),
-            OpenVolume = bar.OpenTick.Volume,
-            OpenAskBaseConversion = bar.OpenTick.AskBaseConversion, OpenBidBaseConversion = bar.OpenTick.BidBaseConversion,
-            OpenAskQuoteConversion = bar.OpenTick.AskQuoteConversion, OpenBidQuoteConversion = bar.OpenTick.BidQuoteConversion,
-            HighAsk = bar.HighTick.Ask, HighBid = bar.HighTick.Bid,
-            HighTimestamp = ((DateTimeOffset)bar.HighTick.Timestamp).ToUnixTimeMilliseconds(),
-            HighVolume = bar.HighTick.Volume,
-            HighAskBaseConversion = bar.HighTick.AskBaseConversion, HighBidBaseConversion = bar.HighTick.BidBaseConversion,
-            HighAskQuoteConversion = bar.HighTick.AskQuoteConversion, HighBidQuoteConversion = bar.HighTick.BidQuoteConversion,
-            LowAsk = bar.LowTick.Ask, LowBid = bar.LowTick.Bid,
-            LowTimestamp = ((DateTimeOffset)bar.LowTick.Timestamp).ToUnixTimeMilliseconds(),
-            LowVolume = bar.LowTick.Volume,
-            LowAskBaseConversion = bar.LowTick.AskBaseConversion, LowBidBaseConversion = bar.LowTick.BidBaseConversion,
-            LowAskQuoteConversion = bar.LowTick.AskQuoteConversion, LowBidQuoteConversion = bar.LowTick.BidQuoteConversion,
-            CloseAsk = bar.CloseTick.Ask, CloseBid = bar.CloseTick.Bid,
-            CloseTimestamp = ((DateTimeOffset)bar.CloseTick.Timestamp).ToUnixTimeMilliseconds(),
-            CloseVolume = bar.CloseTick.Volume,
-            CloseAskBaseConversion = bar.CloseTick.AskBaseConversion, CloseBidBaseConversion = bar.CloseTick.BidBaseConversion,
-            CloseAskQuoteConversion = bar.CloseTick.AskQuoteConversion, CloseBidQuoteConversion = bar.CloseTick.BidQuoteConversion,
-            Volume = bar.Volume
-        });
-    }
-
-    public void SendUpdateTarget(UpdateID update_id, RobotAPI.xTick tick)
-    {
-        SendUpdate(new
-        {
-            UpdateID = (int)update_id,
-            Timestamp = ((DateTimeOffset)tick.Timestamp).ToUnixTimeMilliseconds(),
-            Ask = tick.Ask,
-            Bid = tick.Bid,
-            AskBaseConversion = tick.AskBaseConversion,
-            BidBaseConversion = tick.BidBaseConversion,
-            AskQuoteConversion = tick.AskQuoteConversion,
-            BidQuoteConversion = tick.BidQuoteConversion,
-            Volume = tick.Volume
-        });
+        Send(new[] { (byte)UpdateID.Complete });
     }
 
     public void SendUpdateShutdown()
     {
-        SendUpdate(new { UpdateID = (int)UpdateID.Shutdown });
+        Send(new[] { (byte)UpdateID.Shutdown });
     }
 
-    public string ReceiveAction()
+    public void SendUpdateAccount(IAccount account)
     {
-        while (true)
-        {
-            if (_peer_dead_) throw new InvalidOperationException("Python process exited; cannot receive");
-            if (_socket_.TryReceiveFrameString(_poll_interval_, out var json)) return json;
-        }
+        using var ms = new MemoryStream();
+        ms.WriteByte((byte)UpdateID.Account);
+        WriteString(ms, account.Number.ToString());
+        WriteString(ms, account.IsLive ? "Live" : "Demo");
+        ms.WriteByte((byte)account.AccountType);
+        WriteString(ms, account.Asset.Name);
+        ms.Write(BitConverter.GetBytes(account.Balance), 0, 8);
+        ms.Write(BitConverter.GetBytes(account.Equity), 0, 8);
+        ms.Write(BitConverter.GetBytes(account.Credit), 0, 8);
+        ms.Write(BitConverter.GetBytes(account.PreciseLeverage), 0, 8);
+        ms.Write(BitConverter.GetBytes(account.Margin), 0, 8);
+        ms.Write(BitConverter.GetBytes(account.FreeMargin), 0, 8);
+        ms.Write(BitConverter.GetBytes(account.MarginLevel ?? 0.0), 0, 8);
+        ms.Write(BitConverter.GetBytes(account.StopOutLevel), 0, 8);
+        ms.WriteByte((byte)account.TotalMarginCalculationType);
+        Send(ms.ToArray());
+    }
+
+    public void SendUpdateSymbol(Symbol symbol)
+    {
+        using var ms = new MemoryStream();
+        ms.WriteByte((byte)UpdateID.Security);
+        WriteString(ms, symbol.BaseAsset.Name);
+        WriteString(ms, symbol.QuoteAsset.Name);
+        ms.Write(BitConverter.GetBytes(symbol.Digits), 0, 4);
+        ms.Write(BitConverter.GetBytes(symbol.TickSize), 0, 8);
+        ms.Write(BitConverter.GetBytes(symbol.PipSize), 0, 8);
+        ms.Write(BitConverter.GetBytes(symbol.LotSize), 0, 8);
+        ms.Write(BitConverter.GetBytes(symbol.VolumeInUnitsMin), 0, 8);
+        ms.Write(BitConverter.GetBytes(symbol.VolumeInUnitsMax), 0, 8);
+        ms.Write(BitConverter.GetBytes(symbol.VolumeInUnitsStep), 0, 8);
+        ms.Write(BitConverter.GetBytes(symbol.Commission), 0, 8);
+        ms.WriteByte((byte)symbol.CommissionType);
+        ms.Write(BitConverter.GetBytes(symbol.SwapLong), 0, 8);
+        ms.Write(BitConverter.GetBytes(symbol.SwapShort), 0, 8);
+        ms.WriteByte((byte)symbol.SwapCalculationType);
+        ms.Write(BitConverter.GetBytes(symbol.Swap3DaysRollover != null ? (int)symbol.Swap3DaysRollover : 0), 0, 4);
+        Send(ms.ToArray());
+    }
+
+    public void SendUpdateTick(UpdateID update_id, RobotAPI.xTick tick)
+    {
+        byte[] data = new byte[65];
+        data[0] = (byte)update_id;
+        Buffer.BlockCopy(BitConverter.GetBytes(((DateTimeOffset)tick.Timestamp).ToUnixTimeMilliseconds()), 0, data, 1, 8);
+        Buffer.BlockCopy(BitConverter.GetBytes(tick.Ask), 0, data, 9, 8);
+        Buffer.BlockCopy(BitConverter.GetBytes(tick.Bid), 0, data, 17, 8);
+        Buffer.BlockCopy(BitConverter.GetBytes(tick.AskBaseConversion), 0, data, 25, 8);
+        Buffer.BlockCopy(BitConverter.GetBytes(tick.BidBaseConversion), 0, data, 33, 8);
+        Buffer.BlockCopy(BitConverter.GetBytes(tick.AskQuoteConversion), 0, data, 41, 8);
+        Buffer.BlockCopy(BitConverter.GetBytes(tick.BidQuoteConversion), 0, data, 49, 8);
+        Buffer.BlockCopy(BitConverter.GetBytes(tick.Volume), 0, data, 57, 8);
+        Send(data);
+    }
+
+    private static void WriteSubTick(byte[] buf, int offset, RobotAPI.xTick tick)
+    {
+        Buffer.BlockCopy(BitConverter.GetBytes(((DateTimeOffset)tick.Timestamp).ToUnixTimeMilliseconds()), 0, buf, offset, 8);
+        Buffer.BlockCopy(BitConverter.GetBytes(tick.Ask), 0, buf, offset + 8, 8);
+        Buffer.BlockCopy(BitConverter.GetBytes(tick.Bid), 0, buf, offset + 16, 8);
+        Buffer.BlockCopy(BitConverter.GetBytes(tick.AskBaseConversion), 0, buf, offset + 24, 8);
+        Buffer.BlockCopy(BitConverter.GetBytes(tick.BidBaseConversion), 0, buf, offset + 32, 8);
+        Buffer.BlockCopy(BitConverter.GetBytes(tick.AskQuoteConversion), 0, buf, offset + 40, 8);
+        Buffer.BlockCopy(BitConverter.GetBytes(tick.BidQuoteConversion), 0, buf, offset + 48, 8);
+        Buffer.BlockCopy(BitConverter.GetBytes(tick.Volume), 0, buf, offset + 56, 8);
+    }
+
+    public void SendUpdateBar(UpdateID update_id, RobotAPI.xBar bar)
+    {
+        byte[] data = new byte[337];
+        data[0] = (byte)update_id;
+        Buffer.BlockCopy(BitConverter.GetBytes(((DateTimeOffset)bar.Timestamp).ToUnixTimeMilliseconds()), 0, data, 1, 8);
+        WriteSubTick(data, 9, bar.GapTick);
+        WriteSubTick(data, 73, bar.OpenTick);
+        WriteSubTick(data, 137, bar.HighTick);
+        WriteSubTick(data, 201, bar.LowTick);
+        WriteSubTick(data, 265, bar.CloseTick);
+        Buffer.BlockCopy(BitConverter.GetBytes(bar.Volume), 0, data, 329, 8);
+        Send(data);
+    }
+
+    public void SendUpdateOrder(UpdateID update_id, PendingOrder order)
+    {
+        using var ms = new MemoryStream();
+        ms.WriteByte((byte)update_id);
+        ms.Write(BitConverter.GetBytes(order.Id), 0, 4);
+        ms.WriteByte((byte)order.OrderType);
+        ms.WriteByte((byte)order.TradeType);
+        ms.Write(BitConverter.GetBytes(order.VolumeInUnits), 0, 8);
+        ms.Write(BitConverter.GetBytes(order.TargetPrice), 0, 8);
+        ms.Write(BitConverter.GetBytes(NanIfNull(order.StopLoss)), 0, 8);
+        ms.Write(BitConverter.GetBytes(NanIfNull(order.TakeProfit)), 0, 8);
+        long expMs = order.ExpirationTime.HasValue ? ((DateTimeOffset)order.ExpirationTime.Value).ToUnixTimeMilliseconds() : 0L;
+        ms.Write(BitConverter.GetBytes(expMs), 0, 8);
+        WriteString(ms, order.Label);
+        Send(ms.ToArray());
+    }
+
+    public void SendUpdatePosition(UpdateID update_id, Position position)
+    {
+        using var ms = new MemoryStream();
+        ms.WriteByte((byte)update_id);
+        ms.Write(BitConverter.GetBytes(position.Id), 0, 4);
+        ms.WriteByte((byte)ParsePositionType(position.Comment));
+        ms.WriteByte((byte)position.TradeType);
+        ms.Write(BitConverter.GetBytes(((DateTimeOffset)position.EntryTime).ToUnixTimeMilliseconds()), 0, 8);
+        ms.Write(BitConverter.GetBytes(position.EntryPrice), 0, 8);
+        ms.Write(BitConverter.GetBytes(position.VolumeInUnits), 0, 8);
+        ms.Write(BitConverter.GetBytes(position.Quantity), 0, 8);
+        ms.Write(BitConverter.GetBytes(position.GrossProfit), 0, 8);
+        ms.Write(BitConverter.GetBytes(position.Commissions), 0, 8);
+        ms.Write(BitConverter.GetBytes(position.Swap), 0, 8);
+        ms.Write(BitConverter.GetBytes(position.NetProfit), 0, 8);
+        ms.Write(BitConverter.GetBytes(position.Margin), 0, 8);
+        ms.Write(BitConverter.GetBytes(NanIfNull(position.StopLoss)), 0, 8);
+        ms.Write(BitConverter.GetBytes(NanIfNull(position.TakeProfit)), 0, 8);
+        WriteString(ms, position.Label);
+        Send(ms.ToArray());
+    }
+
+    public void SendUpdateTrade(UpdateID update_id, HistoricalTrade trade)
+    {
+        using var ms = new MemoryStream();
+        ms.WriteByte((byte)update_id);
+        ms.Write(BitConverter.GetBytes(trade.ClosingDealId), 0, 4);
+        ms.Write(BitConverter.GetBytes(trade.PositionId), 0, 4);
+        ms.WriteByte((byte)ParsePositionType(trade.Comment));
+        ms.WriteByte((byte)trade.TradeType);
+        ms.Write(BitConverter.GetBytes(((DateTimeOffset)trade.EntryTime).ToUnixTimeMilliseconds()), 0, 8);
+        ms.Write(BitConverter.GetBytes(((DateTimeOffset)trade.ClosingTime).ToUnixTimeMilliseconds()), 0, 8);
+        ms.Write(BitConverter.GetBytes(trade.EntryPrice), 0, 8);
+        ms.Write(BitConverter.GetBytes(trade.ClosingPrice), 0, 8);
+        ms.Write(BitConverter.GetBytes(trade.VolumeInUnits), 0, 8);
+        ms.Write(BitConverter.GetBytes(trade.Quantity), 0, 8);
+        ms.Write(BitConverter.GetBytes(trade.GrossProfit), 0, 8);
+        ms.Write(BitConverter.GetBytes(trade.Commissions), 0, 8);
+        ms.Write(BitConverter.GetBytes(trade.Swap), 0, 8);
+        ms.Write(BitConverter.GetBytes(trade.NetProfit), 0, 8);
+        WriteString(ms, trade.Label);
+        Send(ms.ToArray());
     }
 }
