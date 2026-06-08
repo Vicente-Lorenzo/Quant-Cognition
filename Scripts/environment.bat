44d71e6149d4fecf373492d6c362dcd0a5a31bcd
Vicente Lorenzo <vicente.aser.lorenzo@gmail.com>
2026-06-08 01:37:32 +0100

    [System] Improved Backtesting System
    Signed-off-by: Vicente Lorenzo <vicente.aser.lorenzo@gmail.com>

--- Scripts/environment.bat
+++ Scripts/environment.bat
@@ -1,21 +1,27 @@
 @echo off
 title Environment Updater
 echo -----------------------------------------------
-echo Updating Conda Environment...
+echo Updating Conda Environments...
 echo -----------------------------------------------
 
-echo [1/1] Executing Mamba update...
 cd /d "C:\Users\Admin\OneDrive\Documents\cAlgo"
-call mamba env update -f Requirements.yml --prune
 
-if %ERRORLEVEL% NEQ 0 (
-    echo -----------------------------------------------
-    echo Update Failed.
-    echo -----------------------------------------------
-    cmd /k
-) else (
-    echo -----------------------------------------------
-    echo Update Complete.
-    echo -----------------------------------------------
-    timeout /t 10 >nul
-)
+echo [1/2] Updating Quant Environment...
+call mamba env update -f Quant.yml --prune
+if %ERRORLEVEL% NEQ 0 goto :failed
+
+echo [2/2] Updating Future Environment...
+call mamba env update -f Future.yml --prune
+if %ERRORLEVEL% NEQ 0 goto :failed
+
+echo -----------------------------------------------
+echo Update Complete.
+echo -----------------------------------------------
+timeout /t 10 >nul
+goto :eof
+
+:failed
+echo -----------------------------------------------
+echo Update Failed.
+echo -----------------------------------------------
+cmd /k
