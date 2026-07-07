0d837d95887fc830a68e41ad8982c6efeb17439e
Vicente Lorenzo <vicente.aser.lorenzo@gmail.com>
2026-07-07 17:25:45 +0100

    [App] Improved App V2
    Signed-off-by: Vicente Lorenzo <vicente.aser.lorenzo@gmail.com>

--- Library/App/V1/Session/Storage.py
+++ Library/App/V1/Session/Storage.py
@@ -2,6 +2,7 @@ from typing_extensions import Self
 from dataclasses import dataclass, field
 
 from Library.Database.Dataclass import DataclassAPI
+from Library.Utility.Typing import MISSING
 
 @dataclass(kw_only=True)
 class StorageAPI(DataclassAPI):
@@ -9,6 +10,9 @@ class StorageAPI(DataclassAPI):
     index: int = field(default=0, init=True, repr=True)
     counter: int = field(default=0, init=True, repr=True)
     total: int = field(default=0, init=True, repr=True)
+
+    def dict(self, *args, **kwargs) -> dict:
+        return {key: value for key, value in super().dict(*args, **kwargs).items() if value is not MISSING}
 
     def trigger(self) -> Self:
         self.index += 1
