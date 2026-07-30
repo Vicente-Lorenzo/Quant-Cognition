165ccff5c19084376ef7f4b9953d174a5dd70d2d
Vicente Lorenzo <vicente.aser.lorenzo@gmail.com>
2026-07-30 18:41:07 +0100

    [Logging] Improved Logging Module
    Signed-off-by: Vicente Lorenzo <vicente.aser.lorenzo@gmail.com>

--- Library/Web/Serve.py
+++ Library/Web/Serve.py
@@ -1,15 +1,15 @@
 from waitress import serve
 
-from Library.Logging import HandlerLoggingAPI, VerboseLevel
+from Library.Logging import LoggingAPI, VerboseLevel
 from Library.Web.App import WebAppAPI
 
 def build() -> WebAppAPI:
     return WebAppAPI(name="Quant Cognition", title="Quant Cognition", team="Vicente Lorenzo", contact="vicente.aser.lorenzo@gmail.com", host="127.0.0.1", port=8050)
 
 def main() -> None:
-    log = HandlerLoggingAPI(Class=WebAppAPI.__name__, Subclass="Serve")
-    log.console.set_verbose_level(VerboseLevel.Debug)
-    log.file.set_verbose_level(VerboseLevel.Debug)
+    log = LoggingAPI(Class=WebAppAPI.__name__, Subclass="Serve")
+    log.console.set_level(VerboseLevel.Debug)
+    log.file.set_level(VerboseLevel.Debug)
     app = build()
     serve(app.app.server, host=app._host_, port=app._port_, threads=8, ident="Quant Cognition")
 
